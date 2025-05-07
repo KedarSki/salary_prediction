cf7a50a275829f91266b9cc8dd8250ab9cdf5eb2
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2025-05-07 22:48:20 +0200

    07.05.2025 - Data fetcher has been created. DAG in Salary pipeline has been added. .env file has been updated

--- airflow/dags/salary_pipeline_dag.py
+++ airflow/dags/salary_pipeline_dag.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 
 from dotenv import load_dotenv
 from src.pipelines_airflow.oracle_loader import load_jobs_from_csv
+from src.pipelines_airflow.data_fetcher import download_csv_from_url
 
 
 load_dotenv()
@@ -21,10 +22,26 @@ with DAG(
     schedule_interval=None,
     start_date=datetime(2025, 4, 1),
     catchup=False,
-) as dag:
+) as dag_load:
 
     def run_loader():
         csv_path = Path(os.getenv("CLEANED_DATA_PATH"))
         load_jobs_from_csv(csv_path)
 
     load_task = PythonOperator(task_id="load_jobs_task", python_callable=run_loader)
+
+with DAG(
+    dag_id="download_data_task",
+    default_args=default_args,
+    description="Download CSV file from ovh server",
+    schedule_interval=None,
+    start_date=datetime(2025, 4, 1),
+    catchup=False,   
+) as dag_download:
+
+    def run_fetcher():
+        url = Path(os.getenv("DATA_SOURCE"))
+        output_path = Path(os.getenv("OUTPUT_PATH"))
+        download_csv_from_url(url, output_path)
+    
+    load_task = PythonOperator(task_id="fetch_csv_file", python_callable=run_fetcher)
--- src/pipelines_airflow/data_fetcher.py
+++ src/pipelines_airflow/data_fetcher.py
@@ -1,4 +1,8 @@
 import os
+import requests
+import logging
+
+import pandas as pd
 
 from pathlib import Path
 
@@ -6,10 +10,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 def download_csv_from_url(url: str, output_path: str):
-    pass
-    
+    try:
+        get_csv = requests.get(url, stream=True)
+        df = pd.read_csv(get_csv)
+        df.to_csv(output_path)
+                   
+    except Exception as e:
+        logger.exception(f"Error server connection to fetch csv file")
+        raise Exception(status_code=500, detail=str(e)) from e
+
+
 if __name__ == "__main__":
-    url = Path(os.getenv("CLEAN_DATA_URL"))
-    output_path = ""
+    url = Path(os.getenv("DATA_SOURCE"))
+    output_path = Path(os.getenv("OUTPUT_PATH"))
     download_csv = download_csv_from_url(url, output_path)
