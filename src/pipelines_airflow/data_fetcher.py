699f1255a4c45b09c3b5a79675b55e64511818ef
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2025-05-18 18:23:02 +0200

    Small corrections made

--- src/pipelines_airflow/data_fetcher.py
+++ src/pipelines_airflow/data_fetcher.py
@@ -11,20 +11,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)-+
 
 def download_csv_from_url(url: str, output_path: str):
     try:
         get_csv = requests.get(url, stream=True)
-        df = pd.read_csv(get_csv)
-        df.to_csv(output_path)
-                   
+        df = pd.read_csv(get_csv.raw)
+        df.to_csv(output_path, index=False)
     except Exception as e:
-        logger.exception(f"Error server connection to fetch csv file")
-        raise Exception(status_code=500, detail=str(e)) from e
-
+        logger.exception("Error server connection to fetch csv file")
+        raise Exception(f"Download failed: {e}") from e
 
 if __name__ == "__main__":
-    url = Path(os.getenv("DATA_SOURCE"))
-    output_path = Path(os.getenv("OUTPUT_PATH"))
-    download_csv = download_csv_from_url(url, output_path)
+    url = os.getenv("DATA_SOURCE")
+    output_path = os.getenv("OUTPUT_PATH")
+    download_csv_from_url(url, output_path)
